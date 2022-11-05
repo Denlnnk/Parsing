9b35db13a023fb1a91b80f60a1405e32a1a4825f
denis <denlnnk2014@gmail.com>
2022-11-05 12:14:02 +0200

    Finished Alf_stats

--- AFL_Stats/afl_stats.py
+++ AFL_Stats/afl_stats.py
@@ -1,4 +1,6 @@
 import json
+
+import pandas as pd
 import requests
 
 url = "https://api.afl.com.au/statspro/playersStats/seasons/CD_S2022014?playerNameLike=&playerPosition=&teamId="
@@ -8,18 +10,47 @@ url = "https://api.afl.com.au/statspro/playersStats/seasons/CD_S2022014?playerNa
 # 3) Paste in Raw text
 # 4) And copy code for Python requests
 headers = {
-  'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-  'x-media-mis-token': 'f72603b067586494b973b07d63fe3379',
-  'Referer': 'https://www.afl.com.au/',
-  'sec-ch-ua-mobile': '?0',
-  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-  'sec-ch-ua-platform': '"Linux"',
-  'Cookie': 'JSESSIONID=975FB6FDDF5426195DBE865744DCC1FE'
+    'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+    'x-media-mis-token': 'f72603b067586494b973b07d63fe3379',
+    'Referer': 'https://www.afl.com.au/',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+    'sec-ch-ua-platform': '"Linux"',
+    'Cookie': 'JSESSIONID=975FB6FDDF5426195DBE865744DCC1FE'
 }
 
 
 def get_data():
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers)
 
-    with open('afl_data.json', 'w') as file:
-        file.write(json.dumps(response, indent=4, ensure_ascii=False))
+    players_data = response.json()
+
+    # with open('afl_data.json', 'w') as file:
+    #     data_json = json.dumps(players_data, indent=4, ensure_ascii=False)
+    #     file.write(data_json)
+
+    players_info_list = []
+    for player in players_data['players']:
+        players_info_list.append({
+            'full_name': player['playerDetails']['givenName'] + player['playerDetails']['surname'],
+            'age': player['playerDetails']['age'],
+            'photoURL': player['playerDetails']['photoURL'],
+            'player_team': player['team']['teamName'],
+            'games_played': player['gamesPlayed']
+        })
+
+    return players_info_list
+
+
+def to_scv(players_data: list):
+    df = pd.DataFrame(players_data)
+    df.to_csv('players_data.csv', index=False)
+
+
+def main():
+    data = get_data()
+    to_scv(data)
+
+
+if __name__ == '__main__':
+    main()
