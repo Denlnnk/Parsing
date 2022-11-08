e213f7c1a467c1a9a3eaafd2bdbee39b03fba93f
denis <denlnnk2014@gmail.com>
2022-11-08 16:06:18 +0200

    Made changes

--- HelmBoots/helm_boots.py
+++ HelmBoots/helm_boots.py
@@ -1,51 +1,54 @@
-import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
-url = 'https://helmboots.com/collections/shop'
-base_url = 'https://helmboots.com/'
-headers = {
-    "user-agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-}
+session = HTMLSession()
+base_url = 'https://helmboots.com'
+
+products_data_list = []
 
 
-def get_all_products_links():
-    response = requests.get(url, headers=headers).content
-    bs4 = BeautifulSoup(response, 'lxml')
+def get_all_product_links():
+    response = session.get('https://helmboots.com/collections/shop')
+    response.html.render(timeout=30)
 
-    all_product = bs4.findAll('div', class_='gc-product')
+    all_products = response.html.find('div.gc-product')
 
-    all_product_links = []
-    for links in all_product:
-        all_product_links.append(base_url + links.find('a', class_='grid-view-item__link')['href'])
-
-    return all_product_links
+    return [base_url + product.find('a.grid-view-item__link', first=True).attrs['href'] for product in all_products]
 
 
-def get_data(products_links: list):
-    products_info_list = []
-    for product_link in products_links[:1]:
-        response = requests.get(product_link, headers=headers).content
-        bs4 = BeautifulSoup(response, 'lxml')
+def get_product_data(product_links: list):
+    count = 1
+    for link in product_links:
+        response = session.get(link)
+        response.html.render(timeout=30)
 
-        images_links = bs4.findAll('div', class_='image-wrap')
-
-        images_links_list = [links.find('img')['src'] for links in images_links]
-
-        print(bs4.find('div', class_='summary-overview'))
-        products_info_list.append({
-            'name': bs4.find('h1', class_='product-single__title libre').text.strip(),
-            'price': bs4.find('span', id='ProductPrice-product-with-banner-template').text.strip(),
-            'images': images_links_list,
-            'rating': 0
+        name = response.html.find('h1.product-single__title.libre', first=True).text
+        price = response.html.find('span#ProductPrice-product-with-banner-template', first=True).text
+        try:
+            rating = response.html.find('span.stamped-summary-text-1', first=True).text
+        except:
+            rating = 'none'
+        try:
+            reviews = response.html.find('span.stamped-summary-text', first=True).text
+        except:
+            reviews = 'none'
+        img_src = base_url + response.html.find('img#FeaturedImage-product-with-banner-template', first=True).attrs['src']
+        products_data_list.append({
+            'name': name,
+            'price': price,
+            'rating': rating,
+            'reviews': reviews,
+            'link': link,
+            'img_src': img_src,
         })
-
-    return products_info_list
+        count += 1
+        print(f'Collected {count} items')
+    print(products_data_list)
+    return products_data_list
 
 
 def main():
-    all_links = get_all_products_links()
-    get_data(all_links)
+    product_links = get_all_product_links()
+    get_product_data(product_links)
 
 
 if __name__ == '__main__':
