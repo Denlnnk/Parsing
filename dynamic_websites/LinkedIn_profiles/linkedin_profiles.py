d7d901fb10f5df3c58beb206081b088c3a0f7206
denis <denlnnk2014@gmail.com>
2022-12-12 07:34:00 +0200

    Implemented Login section

--- dynamic_websites/LinkedIn_profiles/linkedin_profiles.py
+++ dynamic_websites/LinkedIn_profiles/linkedin_profiles.py
@@ -1,11 +1,34 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 from Webdriver_folder.Webdriver_options import Webdriver_options
 
 
 class LinkedIn_profiles:
 
-    def __init__(self, location: str, job_position: str):
+    def __init__(self, location: str = None, job_position: str = None):
         self.username = ''
         self.password = ''
+        self.liked_url = 'https://www.linkedin.com/'
         self.location = location
         self.job_position = job_position
+
+    def parse(self):
+        # Login section
+        driver = webdriver.Chrome(executable_path=Webdriver_options.chromedriver_path(),
+                                  options=Webdriver_options.configuration())
+        driver.get(self.liked_url)
+
+        driver.find_element(By.ID, 'session_key').send_keys(self.username)
+        driver.find_element(By.ID, 'session_password').send_keys(self.password)
+        driver.find_element(By.CLASS_NAME, 'sign-in-form__submit-button').click()
+        time.sleep(4)
+
+
+
+
+if __name__ == '__main__':
+    linked = LinkedIn_profiles(location='North America')
+    linked.parse()
