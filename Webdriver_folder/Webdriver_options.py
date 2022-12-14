24c792558e381e378f1b2fad5bf29702e3cebb42
denis <denlnnk2014@gmail.com>
2022-12-14 09:34:26 +0200

    Implemented Search section

--- Webdriver_folder/Webdriver_options.py
+++ Webdriver_folder/Webdriver_options.py
@@ -1,3 +1,5 @@
+from fp.fp import FreeProxy
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import SoftwareName, OperatingSystem
@@ -6,7 +8,13 @@ from random_user_agent.params import SoftwareName, OperatingSystem
 class Webdriver_options:
 
     @staticmethod
-    def random_user_agent():
+    def get_random_ip():
+        proxy = FreeProxy(country_id=['US'], rand=True, anonym=True).get()
+        ip = proxy.split("://")[1]
+        return ip
+
+    @staticmethod
+    def get_random_user_agent():
         software_names = [SoftwareName.CHROME.value]
         operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
 
@@ -20,12 +28,20 @@ class Webdriver_options:
     @staticmethod
     def configuration():
         chrome_options = Options()
-        chrome_options.add_experimental_option("detach", True)
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-notifications")
-        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option('useAutomationExtension', False)
-        chrome_options.add_argument(f"user-agent={Webdriver_options.random_user_agent()}")
+        # chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # chrome_options.add_experimental_option('useAutomationExtension', False)
+        # chrome_options.add_argument(f'--proxy-server=%{Webdriver_options.get_random_ip()}')
+        # chrome_options.add_argument(f"user-agent={Webdriver_options.get_random_user_agent()}")
+        # webdriver.DesiredCapabilities.CHROME['proxy'] = {
+        #     "httpProxy": Webdriver_options.get_random_ip(),
+        #     "ftpProxy": Webdriver_options.get_random_ip(),
+        #     "sslProxy": Webdriver_options.get_random_ip(),
+        #     "noProxy": None,
+        #     "proxyType": "MANUAL",
+        #     "autodetect": False
+        # }
         return chrome_options
--- dynamic_websites/LinkedIn_profiles/linkedin_profiles.py
+++ dynamic_websites/LinkedIn_profiles/linkedin_profiles.py
@@ -1,7 +1,10 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from Webdriver_folder.Webdriver_options import Webdriver_options
 
@@ -9,8 +12,8 @@ from Webdriver_folder.Webdriver_options import Webdriver_options
 class LinkedIn_profiles:
 
     def __init__(self, location: str = None, job_position: str = None):
-        self.username = ''
-        self.password = ''
+        self.username = 'denlnnk2014@gmail.com'
+        self.password = 'hellomydear12'
         self.liked_url = 'https://www.linkedin.com/'
         self.location = location
         self.job_position = job_position
@@ -24,11 +27,36 @@ class LinkedIn_profiles:
         driver.find_element(By.ID, 'session_key').send_keys(self.username)
         driver.find_element(By.ID, 'session_password').send_keys(self.password)
         driver.find_element(By.CLASS_NAME, 'sign-in-form__submit-button').click()
-        time.sleep(4)
+        WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, 'ember-view.block')))
 
+        # Search section
+        driver.get('https://www.linkedin.com/search/results/people/?origin=SWITCH_SEARCH_VERTICAL&sid=%3A5U')
 
+        if self.location:
+            driver.find_element(
+                By.XPATH,
+                '//button[@aria-label="Locations filter. Clicking this button displays all Locations filter options."]'
+            ).click()
+            time.sleep(1)
+            input_location = driver.find_element(By.XPATH, '//input[@aria-label="Add a location"]')
+            input_location.send_keys(self.location)
+            time.sleep(1)
+            input_location.send_keys(Keys.ARROW_DOWN)
+            input_location.send_keys(Keys.ENTER)
+            time.sleep(1)
+
+            driver.find_element(By.XPATH, '//button[@aria-label="Click to start a search"]').click()
+            time.sleep(3)
+
+        if self.job_position:
+            driver.find_element(By.XPATH, '//button[@aria-label="Click to start a search"]').click()
+            time.sleep(2)
+            input_job_position = driver.find_element(By.XPATH, '//input[@aria-label="Search"]')
+            input_job_position.send_keys(self.job_position)
+            time.sleep(1)
+            input_job_position.send_keys(Keys.ENTER)
 
 
 if __name__ == '__main__':
-    linked = LinkedIn_profiles(location='North America')
+    linked = LinkedIn_profiles(location='United States', job_position='North America')
     linked.parse()
